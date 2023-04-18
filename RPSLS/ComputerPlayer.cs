d8e86b558dbff61bb612e1db939800ce4f4c702b
Chase Loeffler <chaselo609@gmail.com>
2023-04-18 15:55:31 -0400

    Created ComputerPlayer Class. Overwritten ChooseGesture method to randomly pick a gesture for gesture list.

---
+++ RPSLS/ComputerPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class ComputerPlayer : Player
+    {
+        Random rand;
+
+        public ComputerPlayer(string name) : base(name)
+        {
+            rand = new Random();
+        }
+
+        public override void ChooseGesture()
+        {
+            chosenGesture = gestures[rand.Next(5)];
+        }
+    }
+}
