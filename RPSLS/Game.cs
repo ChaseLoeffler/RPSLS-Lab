77249cf46f72d0d2aac7e5e57970d32bcfb31d1a
Chase Loeffler <chaselo609@gmail.com>
2023-04-18 14:59:15 -0400

    Revised WelcomeMessage method, updated ChooseNumberOfHumanPlayers method to prompt user to input the number of players and return the number.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -21,14 +21,17 @@ namespace RPSLS
         //Member Methods (CAN DO)
         public void WelcomeMessage()
         {
-            Console.WriteLine("Welcome to RPSLS! Here are the rules:\nThe game is Best of 3. Ties do not count towards your score.\nYou will be prompeted to chose how many players(either 1 or 2. A single player will face the computer.) and a gesture.\nThe gestures consist of Rock, Paper, Scissors, Lizard, Or Spock.");
+            Console.WriteLine("Welcome to RPSLS! Here are the rules:\nThe game is Best of 3 rounds. Ties do not count towards your score.\nYou will be prompeted to chose how many players(either 1 or 2. A single player will face the computer.) and a gesture.\nThe gestures consist of Rock, Paper, Scissors, Lizard, Or Spock.");
             Console.WriteLine("The gestures will be compared and between them a winner chosen.\nRock wins against Scissors and Lizard.\nPaper wins against Rock and Spock.\nScissors wins against Paper and Lizard\nLizard wins against Paper and Spock\nSpock wins against Scissors and Rock");
             Console.WriteLine("Have fun!");
         }
 
         public int ChooseNumberOfHumanPlayers()
         {
-            return 0;
+            Console.WriteLine("How many Players would you like 1 or 2?");
+            string userInput = Console.ReadLine();
+            int playerNum = Convert.ToInt32(userInput);
+            return playerNum;
         }
 
         public void CreatePlayerObjects(int numberOfHumanPlayers)
