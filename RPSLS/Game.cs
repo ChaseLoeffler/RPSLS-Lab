5178853a45871cbb0364f41891bc888793f76914
Chase Loeffler <chaselo609@gmail.com>
2023-04-18 14:44:58 -0400

    Updated the WelcomeMessage method to write a welcome message and rules for the game to the console.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -21,7 +21,9 @@ namespace RPSLS
         //Member Methods (CAN DO)
         public void WelcomeMessage()
         {
-            Console.WriteLine("Welcome to RPSLS! Here are the rules:\n");
+            Console.WriteLine("Welcome to RPSLS! Here are the rules:\nThe game is Best of 3. Ties do not count towards your score.\nYou will be prompeted to chose how many players(either 1 or 2. A single player will face the computer.) and a gesture.\nThe gestures consist of Rock, Paper, Scissors, Lizard, Or Spock.");
+            Console.WriteLine("The gestures will be compared and between them a winner chosen.\nRock wins against Scissors and Lizard.\nPaper wins against Rock and Spock.\nScissors wins against Paper and Lizard\nLizard wins against Paper and Spock\nSpock wins against Scissors and Rock");
+            Console.WriteLine("Have fun!");
         }
 
         public int ChooseNumberOfHumanPlayers()
